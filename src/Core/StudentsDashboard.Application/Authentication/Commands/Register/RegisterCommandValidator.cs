66790537ecb22211b8e469b1e7822ecfaa7ad113
Jakub Popiołek <grimurin@gmail.com>
2023-11-28 20:38:11 +0100

    Add registration validation
    - Add validation error response
    - Add RegisterCommandValidator
    - Add ValidationBehavior
    - Add FluentValidation to application layer

---
+++ src/Core/StudentsDashboard.Application/Authentication/Commands/Register/RegisterCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace StudentsDashboard.Application.Authentication.Commands.Register;
+
+public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
+{
+    public RegisterCommandValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Email is not valid");
+        RuleFor(x => x.FirstName)
+            .NotEmpty().WithMessage("Firstname is required");
+        RuleFor(x => x.LastName)
+            .NotEmpty().WithMessage("Lastname is required");
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required")
+            .MinimumLength(8).WithMessage("Password must be at least 8 characters long");
+        RuleFor(x => x.ConfirmPassword)
+            .Equal(x => x.Password)
+            .WithMessage("Password and Confirm Password must be the same");
+    }
+}
