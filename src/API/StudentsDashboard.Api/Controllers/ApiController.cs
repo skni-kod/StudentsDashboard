919550ec01682567c76e5e5c906ea3a993ab798c
Jakub Popiołek <grimurin@gmail.com>
2023-11-28 20:38:11 +0100

    Add registration validation
    - Add validation error response
    - Add RegisterCommandValidator
    - Add ValidationBehavior
    - Add FluentValidation to application layer

--- src/API/StudentsDashboard.Api/Controllers/ApiController.cs
+++ src/API/StudentsDashboard.Api/Controllers/ApiController.cs
@@ -1,5 +1,6 @@
 ﻿using ErrorOr;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace StudentsDashboard.Api.Controllers;
 
@@ -8,16 +9,41 @@ public class ApiController : ControllerBase
 {
     protected IActionResult Problem(List<Error> errors)
     {
-        var firstError = errors[0];
+        if (errors.Count is 0)
+        {
+            return Problem();
+        }
+        
+        if (errors.All(error => error.Type == ErrorType.Validation))
+        {
+            return ValidationProblem(errors);
+        }
 
-        var statusCode = firstError.Type switch
+        return Problem(errors[0]);
+    }
+
+    private IActionResult Problem(Error error)
+    {
+        var statusCode = error.Type switch
         {
             ErrorType.Conflict => StatusCodes.Status409Conflict,
             ErrorType.Unauthorized => StatusCodes.Status401Unauthorized,
             ErrorType.NotFound => StatusCodes.Status404NotFound,
+            ErrorType.Validation => StatusCodes.Status400BadRequest,
             _ => StatusCodes.Status500InternalServerError
         };
 
-        return Problem(statusCode: statusCode, title: firstError.Description);
+        return Problem(statusCode: statusCode, title: error.Description);
+    }
+
+    private IActionResult ValidationProblem(List<Error> errors)
+    {
+        var modelStateDictionary = new ModelStateDictionary();
+        foreach (var error in errors)
+        {
+            modelStateDictionary.AddModelError(error.Code, error.Description);
+        }
+
+        return ValidationProblem(modelStateDictionary);
     }
 }
--- src/Core/StudentsDashboard.Application/DependencyInjection.cs
+++ src/Core/StudentsDashboard.Application/DependencyInjection.cs
@@ -1,5 +1,8 @@
-﻿using MediatR;
+﻿using System.Reflection;
+using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using StudentsDashboard.Application.Common.Behaviors;
 
 namespace StudentsDashboard.Application;
 
@@ -9,6 +12,10 @@ public static class DependencyInjection
     {
         services.AddMediatR(configuration => 
             configuration.RegisterServicesFromAssemblies(typeof(DependencyInjection).Assembly));
+
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         return services;
     }
 }
