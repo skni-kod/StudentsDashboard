f8968b9eb4be600ce658395061cd969a7ca4e640
Jakub Popiołek <grimurin@gmail.com>
2024-02-22 18:32:39 +0100

    Add validator for email veryfication

---
+++ src/Core/StudentsDashboard.Application/Authentication/Commands/VerifyEmail/VerifyEmailCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace StudentsDashboard.Application.Authentication.Commands.VerifyEmail;
+
+public class VerifyEmailCommandValidator : AbstractValidator<VerifyEmailCommand>
+{
+    public VerifyEmailCommandValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Email is not valid");
+
+        RuleFor(x => x.Token)
+            .NotEmpty().WithMessage("Token is required")
+            .Length(6).WithMessage("Token must be 6 characters long");
+    }
+}
