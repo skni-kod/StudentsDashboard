3bafac06c2e776fe3bf2598a0173ce9ad0b3af72
unknown <jakubjucha800@gmail.com>
2024-03-02 14:58:01 +0100

    add unit tests for delete event

--- test/UnitTests/StudentsDashboard.Application.UnitTests/TestUtils/Constants/Constants.Events.cs
+++ test/UnitTests/StudentsDashboard.Application.UnitTests/TestUtils/Constants/Constants.Events.cs
@@ -4,6 +4,7 @@ public static partial class Constants
 {
     public static class Events
     {
+        public const int Id = 1;
         public const string Title = "Title";
         public static readonly DateOnly FromDate = DateOnly.Parse("2020-02-12");
         public static readonly TimeOnly FromTime = TimeOnly.Parse("20:00:00");
---
+++ test/UnitTests/StudentsDashboard.Application.UnitTests/WorkEvent/Commands/DeleteWorkEvent/DeleteWorkEventComandHandlerTest.cs
@@ -0,0 +1,74 @@
+﻿using FluentAssertions;
+using Moq;
+using StudentsDashboard.Application.Persistance;
+using StudentsDashboard.Application.UnitTests.WorkEvent.TestUtils;
+using StudentsDashboard.Application.WorkEvents.Commands.DeleteWorkEvent;
+
+namespace StudentsDashboard.Application.UnitTests.WorkEvent.Commands.DeleteWorkEvent;
+
+public class DeleteWorkEventComandHandlerTest
+{
+    private readonly DeleteWorkEventHandler _handler;
+    private readonly Mock<IWorkEventRepository> _mockWorkEvent;
+    private readonly Mock<IUserContextGetIdService> _mockGetUserId;
+
+    public DeleteWorkEventComandHandlerTest()
+    {
+        _mockGetUserId = new Mock<IUserContextGetIdService>();
+        _mockWorkEvent = new Mock<IWorkEventRepository>();
+        _handler = new DeleteWorkEventHandler(_mockGetUserId.Object, _mockWorkEvent.Object);
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnErrorUserDoesNotLogged_WhenTokenJwtIsBad()
+    {
+        //Arrange
+        var deteleCommand = DeleteWorkEventCommandUtils.DeleteWorkEventCommand();
+
+        _mockGetUserId.Setup(x => x.GetUserId)
+            .Returns((int?)null);
+
+        //Act
+        var result = await _handler.Handle(deteleCommand, default);
+
+        //Assert
+        result.IsError.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnOwnerError_WhenUserDidntCreateEvent()
+    {
+        //Arrange
+        var deleteHandler = DeleteWorkEventCommandUtils.DeleteWorkEventCommand();
+
+        _mockGetUserId.Setup(x => x.GetUserId)
+            .Returns(1);
+        
+        _mockWorkEvent.Setup(x => x.HasPermision(It.IsAny<int>(), It.IsAny<int>()))
+            .ReturnsAsync(false);
+
+        //Act
+        var result = await _handler.Handle(deleteHandler, default);
+
+        //Assert
+        result.IsError.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task Hande_Should_ReturnSuccessfull_WhenEventDeleted()
+    {
+        //Arrange
+        var deleteHandler = DeleteWorkEventCommandUtils.DeleteWorkEventCommand();
+        _mockGetUserId.Setup(x => x.GetUserId)
+            .Returns(1);
+
+        _mockWorkEvent.Setup(x => x.HasPermision(It.IsAny<int>(), It.IsAny<int>()))
+            .ReturnsAsync(true);
+
+        //Act
+        var result = await _handler.Handle(deleteHandler, default);
+
+        //Assert
+        result.IsError.Should().BeFalse();
+    }
+}
